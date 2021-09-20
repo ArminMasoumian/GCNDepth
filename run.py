55359e469f5ce01053d5c02114aa461ad2ff834e
ArminMasoumian <74140596+ArminMasoumian@users.noreply.github.com>
2021-09-20 14:41:39 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == '__main__':
+
+    os.system('/home/sconly/Documents/code/py37t11/bin/python -m torch.distributed.launch --master_port=9900 --nproc_per_node=1 train.py --config ./config/cfg_kitti_fm.py --work_dir results')
+    
