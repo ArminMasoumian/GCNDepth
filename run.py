d1f4f347de722bd4d9d5fee08231e74cdfec0247
ArminMasoumian <74140596+ArminMasoumian@users.noreply.github.com>
2022-02-07 14:45:18 +0000

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == '__main__':
 
-    os.system('/home/armin/Documents/code/py37t11/bin/python -m torch.distributed.launch --master_port=9900 --nproc_per_node=1 train.py --config ./config/cfg_kitti_fm.py --work_dir results')
+    os.system('python -m torch.distributed.launch --master_port=9900 --nproc_per_node=1 train.py --config ./config/cfg_kitti_fm.py --work_dir results')
     
